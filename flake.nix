53a32fa4aa34dad9599d6f78273294a601603b6d
Aaron Thapa <notsmart98@gmail.com>
2024-06-24 15:57:30 +0545

    working example

--- flake.nix
+++ flake.nix
@@ -14,27 +14,23 @@
       let
         pkgs = import nixpkgs { inherit system; };
       in with pkgs; rec {
-        devShell = mkShell rec {
-          packages = [
-            pkg-config
-          ];
-          buildInputs = [
-            libxkbcommon
-            libGL
-
-            # WINIT_UNIX_BACKEND=wayland
-            wayland
-
-            # WINIT_UNIX_BACKEND=x11
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXi
-            xorg.libX11
-
-            glfw
-          ];
-          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
-          LIBCLANG_PATH = "${pkgs.llvmPackages_11.libclang.lib}/lib";
-        };
+        devShell = pkgs.mkShell {
+  packages = with pkgs; [
+    rustup
+    cmake
+    clang
+    pkg-config
+    wayland
+    glfw
+  ];
+  LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+    libGL
+    xorg.libXrandr
+    xorg.libXinerama
+    xorg.libXcursor
+    xorg.libXi
+  ];
+  LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+};
       });
 }
